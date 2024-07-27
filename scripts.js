5d7cb15421701bd4e66f4dd5f6ba1ddab457f977
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-07-27 20:15:36 +0400

    v 2.0.0

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ fetch("https://discord.com/api/guilds/1263226066768756787/widget.json")
     return json.json();
   })
   .then(function (user) {
+    // -----------------------------------------------------------------------------------------------
     const statusColor = (statusW) => {
       if (statusW == "online") {
         return "status_h2_online";
@@ -14,11 +15,42 @@ fetch("https://discord.com/api/guilds/1263226066768756787/widget.json")
         return "status_more";
       }
     };
+    // -----------------------------------------------------------------------------------------------
+    const name_checker = (Name) => {
+      if (Name.toUpperCase() == "MAR" || Name.toUpperCase() == "VIN") {
+        return "admin";
+      } else if (Name.toUpperCase() == "SOTK") {
+        return "owner";
+      } else if (
+        Name.toUpperCase() == "TICKET TOOL" ||
+        Name.toUpperCase() == "DYNO" ||
+        Name.toUpperCase() == "OWO" ||
+        Name.toUpperCase() == "ARCANE" ||
+        Name.toUpperCase() == "ROWHOIS"
+      ) {
+        return "BOT";
+      } else {
+        return "userDiv";
+      }
+    };
+    // -----------------------------------------------------------------------------------------------
 
+    const name_role = (role) => {
+      if (role === "admin") {
+        return "🔥★Administrator★";
+      } else if (role === "owner") {
+        return "👑★Owner★";
+      } else if (role === "BOT") {
+        return "🤖BOT";
+      } else {
+        return "member";
+      }
+    };
+    // -----------------------------------------------------------------------------------------------
     for (let i = 0; i < user.members.length; i++) {
-      //////////////////////////////////////////////
+      // -----------------------------------------------------------------------------------------------
       const user_div = document.createElement("div");
-      user_div.classList.add("userDiv");
+      user_div.classList.toggle(name_checker(user.members[i].username));
       const user_div_avatar = document.createElement("div");
       const user_div_avatar_img = document.createElement("img");
       user_div_avatar_img.classList.add("avatar");
@@ -31,13 +63,18 @@ fetch("https://discord.com/api/guilds/1263226066768756787/widget.json")
       server_name_h3.classList.add("server_name_h3");
       const server_name_h3_a = document.createElement("a");
       server_name_h3_a.setAttribute("href", user.instant_invite);
-      //////////////////////////////////////////////
+      const roles = document.createElement("h3");
+      roles.classList.add("roles");
+      // -----------------------------------------------------------------------------------------------
       const usrNum = user.members[i];
       user_div_avatar_img.setAttribute("src", usrNum.avatar_url);
       name.innerHTML = user.members[i].username;
-      status.innerHTML = user.members[i].status;
+      status.innerHTML = `<h6 class="span_role">status:</h6>${user.members[i].status}`;
       server_name_h3_a.innerHTML = user.name;
-      //////////////////////////////////////////////
+      roles.innerHTML = `<h5 class="span_role">ROLE:</h5> ${name_role(
+        name_checker(user.members[i].username)
+      )}`;
+      // -----------------------------------------------------------------------------------------------
       user_div.appendChild(server_name_h3);
       server_name_h3.appendChild(server_name_h3_a);
       main_div.appendChild(user_div);
@@ -45,10 +82,12 @@ fetch("https://discord.com/api/guilds/1263226066768756787/widget.json")
       user_div_avatar.appendChild(user_div_avatar_img);
       user_div.appendChild(name);
       user_div.appendChild(status);
+      user_div.appendChild(roles);
     }
-    ///////////////////////////////////////////////
+    // -----------------------------------------------------------------------------------------------
   });
 function toggleBurgerBar() {
   nav_2_id.classList.add("nav_2_show");
   nav_2_id.classList.toggle("nav_2_none");
 }
+// -----------------------------------------------------------------------------------------------
